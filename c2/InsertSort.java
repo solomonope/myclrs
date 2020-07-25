d95ef4cb005c2dd0434802cacd01eb4507e5924e
opeyemi <solomonope@gmail.com>
2020-07-25 21:31:50 +0000

    insert sort variant 2

--- c2/InsertSort.java
+++ c2/InsertSort.java
@@ -2,6 +2,17 @@ package c2;
 
 public class InsertSort {
  
+    static void s2(int [] data){
+        for (int i =1; i < data.length; i++){
+            int key = data[i];
+            int j =  i-1;
+            while(j >= 0 && key < data[j]){
+                data[j+1] = data[j];
+                j--;
+            }
+            data[j +1] = key;
+        }
+    }
     static void sort(int [] data){
 
         for(int i = 1; i < data.length; i++){
@@ -17,7 +28,7 @@ public class InsertSort {
     public  static void main(String[] args) {
         int [] data =  new int[] {5, 2, 4, 6, 1, 3};
 
-        sort(data);
+        s2(data);
 
         for(int i: data){
             System.out.print(i +",");
